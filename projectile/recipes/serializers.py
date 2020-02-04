4294a25e574d29e66fc96f8dd29f939385add5a7
ayezac <ayeshazac@gmail.com>
2020-02-04 10:37:44 +0100

    Make image a base64imagefield

--- projectile/recipes/serializers.py
+++ projectile/recipes/serializers.py
@@ -1,7 +1,9 @@
 from .models import Recipe, Ingredient, SavedRecipe
+import json
 from django.contrib.auth.models import User
 from user.serializers import UserSerializer
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -12,6 +14,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(source='ingredient_set', many=True)
     author = UserSerializer(read_only=True)
+    image = Base64ImageField()
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'author', 'pub_date', 'ingredients', 'method', 'image']
